dce228e8c464bf9b97fb5ba11defd052aa3e4fcc
nicoelias997 <nicoelias997@gmail.com>
2022-09-08 20:32:10 +0200

    agregamos la conf desde firebase, la inicializamos y creamos 2 variables de la base de datos y la auth = autenticacion

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import app from "firebase/app" //con esto config firebase
+import "firebase/compat/firestore"
+import "firebase/compat/auth"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBO3COeWxbxDTIiQW6RTCLYpBZxKyo1XHw",
+    authDomain: "crud-databae.firebaseapp.com",
+    projectId: "crud-databae",
+    storageBucket: "crud-databae.appspot.com",
+    messagingSenderId: "14099606275",
+    appId: "1:14099606275:web:ab39c5960203e883e11736"
+  };
+  
+app.initializeApp(firebaseConfig);
+
+const dataBase = firebase.firestore()
+const auth = app.auth()
+
+export {dataBase, auth}
