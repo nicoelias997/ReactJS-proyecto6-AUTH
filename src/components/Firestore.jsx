52ac791820b841941b1ce83f2110485942e70c05
nicoelias997 <nicoelias997@gmail.com>
2022-09-15 19:40:38 +0200

    Agregamos el codigo del componente App de administrador de tareas que hicimos en el proy5-crud para que cada usuario tenga su propia lista de tareas y lo arreglamos

--- src/components/Firestore.jsx
+++ src/components/Firestore.jsx
@@ -1,13 +1,151 @@
-import React from 'react'
+import React from "react";
+import {dataBase} from "../firebase"
+function Firestore(props) {
+  
+  const [tareas, setTareas] = React.useState([])
+  const [tarea, setTarea] = React.useState('')
+  const [modoEdicion, setModoEdicion] = React.useState(false)
+  const [id, setId] = React.useState("")
 
-const Firestore = (props) => {
-  return (
-    <div>
-        <h1>
-        Firestore
-        </h1>
-    </div>
-  )
+
+  React.useEffect(() => {
+  
+      const obtenerDatos = async () => {
+          try {
+              const data = await dataBase.collection(props.user.uid).get()
+              const arrayData = data.docs.map(doc => ({id: doc.id, ...doc.data()}))
+              console.log(arrayData) 
+              setTareas(arrayData)     
+          } catch (error) {
+              console.log(error)
+          }
+      }
+      obtenerDatos()
+  
+  }, [props.user.uid])
+
+
+const agregar = async (e) => {
+    e.preventDefault()
+    if(!tarea.trim()){
+        console.log('sin texto')
+        return
+    }
+    try{
+      const nuevaTarea = {
+        name: tarea,
+        fecha: Date.now()
+      }
+      const data = await dataBase.collection(props.user.uid).add(nuevaTarea)
+      setTareas([
+        ...tareas,
+        {
+          ...nuevaTarea, id: data.id
+        }
+      ])
+      setTarea("")
+
+    } catch(error){
+      console.log(error)
+    }
+    console.log(tarea)
 }
 
-export default Firestore
+const eliminar = async (id) => {
+  try {
+    await dataBase.collection(props.user.uid).doc(id).delete()
+    const arrayFiltrado = tareas.filter(item => item.id !== id)
+    setTareas(arrayFiltrado)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const activarEdicion = (item) => {
+  setModoEdicion(true)
+  setTarea(item.name)
+  setId(item.id)
+}
+
+const editar = async (e) => {
+  e.preventDefault()
+  if(!tarea.trim()){
+    console.log('vacio')
+    return
+  }
+  try {
+    await dataBase.collection(props.user.uid).doc(id).update({
+      name: tarea
+    })
+    const arrayEditado = tareas.map(item => (
+      item.id === id ? {id: item.id, fecha: item.fecha, name: tarea} : item
+    ))
+    setTareas(arrayEditado)
+    setModoEdicion(false)
+    setId('')
+    setTarea('')
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+  return (
+
+    <div className="container mb-2">
+    <div className="row">
+        <div className="col-md-6">
+            <h3>Lista de Tareas</h3>
+            <ul className="list-group">
+            {
+                tareas.map(item => (
+                <li className="list-group-item" key={item.id}>
+                  <span>{item.name}</span>
+          <button 
+            className="btn btn-danger btn-sm float-end"
+            onClick={() => eliminar(item.id)}>
+            Eliminar
+        </button>
+        <button 
+    className="btn btn-warning btn-sm float-end me-2"
+    onClick={() => activarEdicion(item)}
+>
+    Editar
+</button>
+                </li>
+                ))
+            }
+            </ul>
+        </div>
+        <div className="col-md-6">
+    <h3>
+    {
+        modoEdicion ? 'Editar Tarea' : 'Agregar Tarea'
+    }
+    </h3>
+    <form onSubmit={modoEdicion ? editar : agregar}>
+    <input 
+        type="text" 
+        className="form-control mb-2"
+        placeholder='Ingrese Tarea'
+        value={tarea}
+        onChange={e => setTarea(e.target.value)}
+    />
+    <button 
+        type='submit'
+        className={
+        modoEdicion ? 'btn btn-warning col-12 btn-sm' : 
+        'btn btn-dark col-12 btn-sm'
+        }
+    >
+        {
+        modoEdicion ? 'Editar' : 'Agregar'
+        }
+    </button>
+    </form>
+</div>
+    </div>
+</div>
+  );
+}
+
+export default Firestore;
